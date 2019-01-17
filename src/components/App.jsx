3df305a465d7ccdcf8e343caa5883dbafd75eae4
Kyle Davies <kdavies311@gmail.com>
2019-01-16 20:14:43 -0800

    got our videoList display up. currently displaying the same video 5 times

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
+import exampleVideoData from "../data/exampleVideoData.js";
+import VideoList from "./VideoList.js";
+import VideoPlayer from "./VideoPlayer.js";
+
+
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,7 +16,7 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList videos={exampleVideoData}/>
       </div>
     </div>
   </div>
